45ab4a2c6f245a506c1db95ebff58eb36510ef96
alex <766f6964+github@gmail.com>
2023-08-06 12:11:12 +0200

    added local & remote tests

--- src/Test/ThumbnailTest.php
+++ src/Test/ThumbnailTest.php
@@ -40,8 +40,39 @@ class ThumbnailTest extends \PHPUnit\Framework\TestCase
     {
     }
 
-    public function testSkip(): void
+    public function testRemoteNotExistentImage(): void
     {
-        $this->markTestSkipped("empty test");
+        $thumbnail = new \aportela\RemoteThumbnailCacheWrapper\Thumbnail(self::$logger, sys_get_temp_dir());
+        $url = "https://raw.githubusercontent.com/aportela/remote-thumbnail-cache-wrapper/2.0pre/src/Test/404.jpg";
+        $this->assertFalse($thumbnail->getFromRemoteURL($url, 64, 43));
+        $this->assertEmpty($thumbnail->path);
+    }
+
+    public function testRemoteExistentImage(): void
+    {
+        $thumbnail = new \aportela\RemoteThumbnailCacheWrapper\Thumbnail(self::$logger, sys_get_temp_dir());
+        // image credits: https://unsplash.com/es/fotos/89xuP-XmyrA (Caspar Camille Rubin)
+        $url = "https://raw.githubusercontent.com/aportela/remote-thumbnail-cache-wrapper/2.0pre/src/Test/200.jpg";
+        $this->assertTrue($thumbnail->getFromRemoteURL($url, 64, 43));
+        $this->assertNotEmpty($thumbnail->path);
+        $this->assertFileExists($thumbnail->path);
+    }
+
+    public function testLocalNotExistentImage(): void
+    {
+        $thumbnail = new \aportela\RemoteThumbnailCacheWrapper\Thumbnail(self::$logger, sys_get_temp_dir());
+        $localFile = sprintf("%s%s%s", __DIR__, DIRECTORY_SEPARATOR, "400.jpg");
+        $this->assertFalse($thumbnail->getFromLocalFilesystem($localFile, 64, 43));
+        $this->assertEmpty($thumbnail->path);
+    }
+
+    public function testLocalExistentImage(): void
+    {
+        $thumbnail = new \aportela\RemoteThumbnailCacheWrapper\Thumbnail(self::$logger, sys_get_temp_dir());
+        // image credits: https://unsplash.com/es/fotos/89xuP-XmyrA (Caspar Camille Rubin)
+        $localFile = sprintf("%s%s%s", __DIR__, DIRECTORY_SEPARATOR, "200.jpg");
+        $this->assertTrue($thumbnail->getFromLocalFilesystem($localFile, 64, 43));
+        $this->assertNotEmpty($thumbnail->path);
+        $this->assertFileExists($thumbnail->path);
     }
 }
